c0bc0c5e566474ce289c8d96e861e0b0ad3c2516
tanxi <tanxi@zencloud.net.cn>
2019-09-29 14:28:10 +0800

    init

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -10,6 +10,7 @@ public class DemoApplication {
         SpringApplication.run(DemoApplication.class, args);
 
         System.out.println("1231423123");
+        System.out.println("测试第二次 dev");
     }
 
 }
